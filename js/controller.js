a107b2850ff1e38526860307b201237618329ac7
Sk Jhohev <sjhoh@amazon.com>
2019-06-03 13:43:49 +0530

    Added About section

--- js/controller.js
+++ js/controller.js
@@ -1,12 +1,20 @@
 const controller = ($scope, $http) => {
     $scope.year = new Date().getFullYear();
     $scope.catalogue = [];
+    $scope.about = [];
 
     $http
         .get('res/catalogue.json')
         .then(responseDataTx)
         .then((catalogue) => {
             $scope.catalogue = catalogue;
+        });
+
+    $http
+        .get('res/aboutt.json')
+        .then(responseDataTx)
+        .then((about) => {
+            $scope.about = about;
         });
 }
 
