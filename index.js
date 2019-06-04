fa50d4ee91714e2b75b81f94b8547e473e4aa4b4
Sk Jhohev <sjhoh@amazon.com>
2019-06-04 18:04:34 +0530

    Added prerenderer

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 const express = require('express');
+const prerenderer = require('prerender-node');
 
 const isProd = () => process.env.NODE_ENV && process.env.NODE_ENV === 'production';
 
 const app = express();
-const port = isProd()? 80: 3000;
-
+app.use(prerenderer.set('prerenderToken', 'KE4qBfLFkUEPgYu9Ksmy'));
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html');
 });
 
+const port = isProd()? 80: 3000;
 app.listen(port, () => console.log(`Server running on port ${port}`));
